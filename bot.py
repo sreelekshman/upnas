c45252955cc3c39abc1e67a6632411cb09d8dabc
sreelekshman <sreelekshmanchunakara@gmail.com>
2024-07-01 12:13:39 +0530

    bot: Remove force replay of ```/start``` command
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- bot.py
+++ bot.py
@@ -76,7 +76,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
     await update.message.reply_html(
         rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
     )
 
 async def ping(update, context):
